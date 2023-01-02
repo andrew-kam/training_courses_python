2522f34b687fcd94f5f0db566bf8f4662001cded
ANDR <andr@example.com>
2023-01-02 11:51:45 +0300

    update 2_4_8

--- stepik_auto_testing/part_2/2_4_8_EC_task.py
+++ stepik_auto_testing/part_2/2_4_8_EC_task.py
@@ -1,11 +1,12 @@
+import time
+import math
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import math
 
 
 def calc(x):
