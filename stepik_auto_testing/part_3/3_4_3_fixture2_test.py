fb9e6ad40fde3f1676d768838741f8ebc83ab008
ANDR <andr@example.com>
2023-01-05 12:09:47 +0300

    init 3_4_3

---
+++ stepik_auto_testing/part_3/3_4_3_fixture2_test.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+link = "https://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    return browser
+
+
+class TestMainPage1:
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
+if __name__ == "__main__":
+    pytest.main()
+# https://stepik.org/lesson/237257/step/3?auth=login&unit=209645
