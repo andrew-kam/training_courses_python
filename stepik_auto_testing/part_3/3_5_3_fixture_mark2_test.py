e8b222de191a3032f849bddff0fe4d0fc1399e88
ANDR <andr@example.com>
2023-01-06 18:36:10 +0300

    init 3_5_3

---
+++ stepik_auto_testing/part_3/3_5_3_fixture_mark2_test.py
@@ -0,0 +1,40 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+link = "https://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+        print(111)
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+        print(222)
+
+
+if __name__ == "__main__":
+    pytest.main()
+    # pytest -s -v -m "smoke and win10" stepik_auto_testing/part_3/3_5_3_fixture_mark2_test.py
+
+# https://stepik.org/lesson/236918/step/3?auth=login&unit=209305
