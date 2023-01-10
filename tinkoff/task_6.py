7ca9d7b9005534ac52c2c587c1e92dc4d4226594
Andre <andre@mail.com>
2023-01-10 19:50:15 +0300

    refactor task_6

--- tinkoff/task_6.py
+++ tinkoff/task_6.py
@@ -8,6 +8,16 @@ def height(list_height: list[int]):
     return list_chet, list_no
 
 
+def check(list_items: list, ind_chet: int, ind_no: int):
+    list_new = list_items[:]
+    list_new[ind_chet], list_new[ind_no] = list_new[ind_no], list_new[ind_chet]
+    for i in range(len(list_new)):
+        if i % 2 == 0 and list_new[i] % 2 == 0\
+                or i % 2 == 1 and list_new[i] % 2 == 1:
+            return False
+    return True
+
+
 if __name__ == '__main__':
     number_students = int(input())
     list_height_students = list(map(int, input().split()))
@@ -15,6 +25,7 @@ if __name__ == '__main__':
     l_chet, l_no = height(list_height_students)
 
     if len(l_chet) == len(l_no) == 1:
-        print(*l_chet, *l_no)
+        if check(list_height_students, l_chet[0] - 1, l_no[0] - 1):
+            print(*l_chet, *l_no)
     else:
         print(-1, -1)
