b8de7f6882b2340c9bb7dfb60529a72fecaa8ea6
Andre <andre@mail.com>
2023-01-11 15:25:16 +0300

    add swap, read_input, change checker task_7

--- tinkoff/task_7.py
+++ tinkoff/task_7.py
@@ -33,53 +33,24 @@ def double(students: list[int]):
     return list_double, list_lack, indexes_double
 
 
+def swap(index, lack, students):
+    students_new = students[:]
+    students_new[index] = lack
+    return students_new
+
+
 def main():
-    # len_student, students = read_input()
-    students = [2, 3, 4, 5, 6, 4]
-    print(checker(students))
+    len_student, students = read_input()
+
     list_double, list_lack, indexes_double = double(students)
-    print(double(students))
-    if len(indexes_double) == 2:
-        index_1 = indexes_double[0]
-        index_2 = indexes_double[1]
-        students_new_1 = students[:]
-        students_new_1[index_1] = list_lack[0]
-        students_new_2 = students[:]
-        students_new_2[index_2] = list_lack[0]
-        if checker(students_new_1):
-            print(index_1 + 1, list_lack[0])
-        elif checker(students_new_2):
-            print(index_2 + 1, list_lack[0])
-        else:
-            print(-1, -1)
+
+    if len(indexes_double) == 2 and checker(swap(indexes_double[0], list_lack[0], students)):
+        print(indexes_double[0] + 1, list_lack[0])
+    elif len(indexes_double) == 2 and checker(swap(indexes_double[1], list_lack[0], students)):
+        print(indexes_double[1] + 1, list_lack[0])
+    else:
+        print(-1, -1)
 
 
 if __name__ == '__main__':
     main()
-
-    # l_double, l_not_in, l_index = double(list_students)
-    # if len(l_double) == 1:
-    #     y = l_not_in[0]
-    #     ind_1 = l_index[0]
-    #     ind_2 = l_index[1]
-    #
-    #     list_new_1 = list_students[:]
-    #     list_new_1[ind_1] = y
-    #
-    #     list_new_2 = list_students[:]
-    #     list_new_2[ind_2] = y
-    #
-    #     if check_cycle(list_new_1):
-    #         print(ind_1 + 1, y)
-    #
-    #     elif check_cycle(list_new_2):
-    #         print(ind_2 + 1, y)
-    #     else:
-    #         print(-1, -1)
-    # else:
-    #     print(-1, -1)
-
-# list_students = [2, 3, 4, 6, 1, 1]
-# print(check_cycle(list_students))
-# print(check_cycle(list_new_1))
-# print(check_cycle(list_new_2))
