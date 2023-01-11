b19c6e30688a3c662cfbf836de0dc9d204b5a4d0
Andre <andre@mail.com>
2023-01-11 10:58:57 +0300

    add output [1, 3] and refactor task_6

--- tinkoff/task_6.py
+++ tinkoff/task_6.py
@@ -1,31 +1,32 @@
-def height(list_height: list[int]):
-    list_chet, list_no = [], []
-    for i in range(len(list_height)):
-        if i % 2 == 0 and list_height[i] % 2 == 0:
-            list_chet.append(i+1)
-        if i % 2 == 1 and list_height[i] % 2 == 1:
-            list_no.append(i+1)
-    return list_chet, list_no
+def read_input() -> tuple[int, list[int]]:
+    input_len = int(input().strip())
+    input_students = list(map(int, input().strip().split()))
+    return input_len, input_students
 
 
-def check(list_items: list, ind_chet: int, ind_no: int):
-    list_new = list_items[:]
-    list_new[ind_chet], list_new[ind_no] = list_new[ind_no], list_new[ind_chet]
-    for i in range(len(list_new)):
-        if i % 2 == 0 and list_new[i] % 2 == 0\
-                or i % 2 == 1 and list_new[i] % 2 == 1:
-            return False
-    return True
+def get_answer(students: list[int]) -> list[int]:
+    list_even, list_odd = [], []
+    for i in range(len(students)):
+        if i % 2 == 0 and students[i] % 2 == 0:
+            list_even.append(i + 1)
+        if i % 2 == 1 and students[i] % 2 == 1:
+            list_odd.append(i + 1)
+
+    if len(list_odd) == len(list_even) == 0\
+            and len(students) >= 3:
+        return [1, 3]
+    elif len(list_odd) == len(list_even) == 1:
+        return [list_odd[0], list_even[0]]\
+            if list_odd[0] < list_even[0]\
+            else [list_even[0], list_odd[0]]
+    else:
+        return [-1, -1]
+
+
+def main():
+    len_student, students = read_input()
+    print(*get_answer(students))
 
 
 if __name__ == '__main__':
-    number_students = int(input())
-    list_height_students = list(map(int, input().split()))
-
-    l_chet, l_no = height(list_height_students)
-
-    if len(l_chet) == len(l_no) == 1:
-        if check(list_height_students, l_chet[0] - 1, l_no[0] - 1):
-            print(*l_chet, *l_no)
-    else:
-        print(-1, -1)
+    main()
