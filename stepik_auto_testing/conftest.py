d101ce14fb1ae8ea2048826771a14caeb07922c7
Andre <andre@mail.com>
2023-01-16 12:01:35 +0300

    init conftest.py

---
+++ stepik_auto_testing/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
