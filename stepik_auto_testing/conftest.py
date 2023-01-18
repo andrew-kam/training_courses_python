29cfa28a2a57ab1093c2c8f46709dc505e766860
Andre <andre@mail.com>
2023-01-18 18:28:55 +0300

    add firefox

--- stepik_auto_testing/conftest.py
+++ stepik_auto_testing/conftest.py
@@ -2,7 +2,9 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
+
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
 
 
 @pytest.fixture(scope="function")
@@ -12,4 +14,14 @@ def browser():
     browser.implicitly_wait(20)
     yield browser
     print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.fixture(scope="function")
+def browser_f():
+    print("\nstart browser_f for test..")
+    browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    browser.implicitly_wait(20)
+    yield browser
+    print("\nquit browser_f..")
     browser.quit()
