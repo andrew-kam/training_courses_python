8c44322625dc88ad5cd819a1e3105465fbde235d
Andre <andre@mail.com>
2023-01-25 10:58:54 +0300

    refactor conftest

--- stepik_auto_testing/conftest.py
+++ stepik_auto_testing/conftest.py
@@ -6,24 +6,41 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: es/fr/ru ...")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    # browser = None
+    user_language = request.config.getoption("language")
+    browser = None
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        options = OptionsChrome()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})
+
+        browser = webdriver.Chrome(options=options,
+                                   service=ChromeService(ChromeDriverManager().install()))
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", user_language)
+
+        browser = webdriver.Firefox(options=options_firefox,
+                                    service=FirefoxService(GeckoDriverManager().install()))
+    # else:
+    #     raise pytest.UsageError("--browser_name should be chrome or firefox")
     browser.implicitly_wait(10)
     yield browser
     print("\nquit browser..")
