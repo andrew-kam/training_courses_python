7bb1915c74d63ebd7c226162f586edeab9030d09
Andre <andre@mail.com>
2023-01-30 10:11:15 +0300

    default en

--- stepik_auto_testing/conftest.py
+++ stepik_auto_testing/conftest.py
@@ -14,7 +14,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: es/fr/ru ...")
 
 
