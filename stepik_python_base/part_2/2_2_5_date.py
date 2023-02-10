bdd3bdb5216d41b84c833f0bb5c06b48606bac46
Andre <andre@mail.com>
2023-02-10 08:16:13 +0300

    refactor, add strip

--- stepik_python_base/part_2/2_2_5_date.py
+++ stepik_python_base/part_2/2_2_5_date.py
@@ -1,7 +1,22 @@
 import datetime
 
-g, m, d = map(int, input().split())
-delta = datetime.timedelta(days=int(input()))
-data = datetime.date(g, m, d)
-new_data = data + delta
-print(new_data.year, new_data.month, new_data.day)
+
+def read_input():
+    year, month, day = map(int, input().strip().split())
+    delta = datetime.timedelta(days=int(input().strip()))
+    return year, month, day, delta
+
+
+def get_answer(year, month, day, delta):
+    current_date = datetime.date(year, month, day)
+    new_date = current_date + delta
+    return new_date
+
+
+def main():
+    answer_date = get_answer(*read_input())
+    print(answer_date.year, answer_date.month, answer_date.day)
+
+
+if __name__ == '__main__':
+    main()
