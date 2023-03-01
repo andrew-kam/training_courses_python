2228d4804338dc68ec9b76ea77abbc84fc5196d3
Andre <andre@mail.com>
2023-03-01 09:19:01 +0300

    rename

--- stepik_python_base/part_3/3_1_7_startswith.py
+++ stepik_python_base/part_3/3_1_7_startswith.py
@@ -1,7 +1,8 @@
 s, t = (input() for _ in range(2))
 n = 0
 for i in range(len(s)):
-#    if t == s[i:i+len(t)]:
     if s[i:].startswith(t):
         n += 1
 print(n)
+
+# https://stepik.org/lesson/24469/step/7?unit=6775
