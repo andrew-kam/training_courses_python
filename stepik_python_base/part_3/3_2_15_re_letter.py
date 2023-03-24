3bc379b76dd7ca249f01279891d70acc4351bafa
ANDR <andr@example.com>
2023-03-24 09:07:07 +0300

    rename and add href

--- stepik_python_base/part_3/3_2_15_re_letter.py
+++ stepik_python_base/part_3/3_2_15_re_letter.py
@@ -5,3 +5,5 @@ for line in sys.stdin:
     line = line.strip()
     fixed_typos = re.sub(r'(\w)\1+', r'\1', line)
     print(fixed_typos)
+
+# https://stepik.org/lesson/24470/step/15?auth=login&unit=6776
